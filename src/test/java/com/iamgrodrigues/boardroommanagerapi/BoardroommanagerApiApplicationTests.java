0932993c916e9119b9185dd50e89b6e74abab69f
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-12 16:07:45 -0300

    docs: Remove files according to the new gitignore

---
+++ src/test/java/com/iamgrodrigues/boardroommanagerapi/BoardroommanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamgrodrigues.boardroommanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardroommanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
