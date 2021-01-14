0af5c436ec6472f6dfe620cd0ccfa21fd4cae680
iamgrodrigues <iamgrodrigues@gmail.com>
2021-01-13 23:42:51 -0300

    wip: Update crossOrigin

--- src/main/java/com/iamgrodrigues/boardroommanagerapi/controller/BoardroomController.java
+++ src/main/java/com/iamgrodrigues/boardroommanagerapi/controller/BoardroomController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://boardroommanager.netlify.app")
 @RequestMapping("/api/v1")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BoardroomController {
